acd765ec5e6ee54b8f62f05969372b45036d0d8b
johngarrett718 <john.garrett718@gmail.com>
2024-02-25 20:11:17 -0600

    update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { BlogPostContainer } from './modules/blogPost/'
+import { BlogPostContainer } from './modules/blogPost'
 
 const App = () => {
   return (
