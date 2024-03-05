09453a1b1e963bedd3024c6c64e40cebfc1e3760
johngarrett718 <john.garrett718@gmail.com>
2024-03-04 18:50:09 -0600

    final commit

--- src/index.tsx
+++ src/index.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
       element: <Root />,
       children: [
         {
-          path: '/',
+          index: true,
           element: <AboutMe />
         },
         {
